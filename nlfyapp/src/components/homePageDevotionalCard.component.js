de4df8f22084c4d54d86cbfb4bb943209547d981
ria4github <ria.guhat@gmail.com>
2023-02-10 10:58:13 +0530

    Trimming of content

--- nlfyapp/src/components/homePageDevotionalCard.component.js
+++ nlfyapp/src/components/homePageDevotionalCard.component.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Card, Text } from "react-native-paper";
 import { LinearGradient } from "expo-linear-gradient";
+import { View } from "react-native";
 
 const DevotionalCard = styled(Card)`
   top: 40px;
@@ -25,11 +26,12 @@ const CardContent = styled(Text)`
   font-size: ${(props) => props.theme.fontSizes.caption};
   font-weight: ${(props) => props.theme.fontWeights.regular};
   font-family: ${(props) => props.theme.fonts.body};
+  width: 340px;
 `;
 
 const CardReadmore = styled(Text)`
   position: absolute;
-  padding-left: 100px;
+  padding-left: 240px;
   top: 150px;
   color: ${(props) => props.theme.colors.text.inverse};
   font-size: ${(props) => props.theme.fontSizes.caption};
@@ -44,6 +46,9 @@ const StyledLinearGradient = styled(LinearGradient)`
 `;
 
 export const HomePageDevotionalCard = ({ devotional = {} }) => {
+  const cardContentText =
+    "Your road led through the sea, your pathway through the mighty waters - a pathway no one knew was there - Psalms 77:19(NLT) Godactively works through your circumstances. But you cannot judge your situation apart from God’s";
+  const cardContentTitle = "God's Plan for you is great - Rick Warren";
   return (
     <DevotionalCard>
       <StyledLinearGradient
@@ -52,14 +57,9 @@ export const HomePageDevotionalCard = ({ devotional = {} }) => {
         colors={["#E94A27", "#F26924"]}
       >
         <Card.Content>
-          <CardTitle variant="titleLarge">
-            "God's Plan for you is great" - Rick Warren
-          </CardTitle>
-          <CardContent variant="bodyMedium">
-            Your road led through the sea, your pathway through the mighty
-            waters - a pathway no one knew was there” - Psalms 77:19(NLT) God
-            actively works through your circumstances. But you cannot judge your
-            situation apart from God’s
+          <CardTitle variant="titleLarge">{cardContentTitle}</CardTitle>
+          <CardContent numberOfLines={5} variant="bodyMedium">
+            {cardContentText}
           </CardContent>
           <CardReadmore variant="bodyMedium">Read More...</CardReadmore>
         </Card.Content>
