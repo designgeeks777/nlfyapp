0778432bfb11e255f556ea39fcff98505f95ca8d
Emmanuel <emmanuelpalli@gmail.com>
2023-02-10 10:30:38 +0530

    Async-storageData

--- nlfyapp/App.js
+++ nlfyapp/App.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Onboarding } from "./src/features/onboarding.screen";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
 import { useFonts, Lato_400Regular } from "@expo-google-fonts/lato";
 import { theme } from "./src/infrastructure/theme";
+import {
+  gettingData,
+  storingData,
+} from "./src/components/asyncstorage.component";
+import { Home } from "./src/features/home.screen";
 
-export const App = () => {
-  const [latoLoaded] = useFonts({
-    Lato_400Regular,
-  });
+const App = () => {
+  const [hasLaunched, setHasLaunched] = useState(false);
+  const [latoLoaded] = useFonts({ Lato_400Regular });
+  const HAS_LAUNCHED = "HAS_LAUNCHED";
+
+  useEffect(() => {
+    const getData = async () => {
+      const storedData = await gettingData(HAS_LAUNCHED);
+      if (storedData) {
+        setHasLaunched(true);
+      } else {
+        await storingData(HAS_LAUNCHED, "true");
+      }
+    };
+
+    getData().catch((error) => {
+      console.log(error);
+    });
+  }, []);
 
   if (!latoLoaded) {
     return null;
@@ -17,7 +37,7 @@ export const App = () => {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Onboarding />
+        {hasLaunched ? <Home /> : <Onboarding />}
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
