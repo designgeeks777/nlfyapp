1dcb989fd1c1d4de69944db3426fd0c91cb93beb
ria4github <ria.guhat@gmail.com>
2023-02-14 12:02:35 +0530

    Enabled prettier

--- nlfyapp/src/features/component/homeScreenComponent/sermonVideo.component.js
+++ nlfyapp/src/features/component/homeScreenComponent/sermonVideo.component.js
@@ -1,39 +1,38 @@
-/* eslint-disable prettier/prettier */
 import React from "react";
 import { Image, View, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 const styles = StyleSheet.create({
-    videoContainer: {
-        top: 320,
-        position: "absolute",
-        width: 360,
-        height: 200,
-        borderRadius: 30,
-    },
-    video: {
-        width: "100%",
-        height: "100%",
-        borderRadius: 30,
-    },
-    playButton: {
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: [{ translateX: -36.5 }, { translateY: -38.5 }],
-    },
+  videoContainer: {
+    top: 320,
+    position: "absolute",
+    width: 360,
+    height: 200,
+    borderRadius: 30,
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 30,
+  },
+  playButton: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: [{ translateX: -36.5 }, { translateY: -38.5 }],
+  },
 });
 
 export const SermonVideo = () => {
-    return (
-        <View style={styles.videoContainer}>
-            <Image
-                style={styles.video}
-                source={require("nlfyapp/assets/blackscreen.jpg")}
-            />
-            <View style={styles.playButton}>
-                <Ionicons name="play-circle-sharp" size={65} color="#F26924" />
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.videoContainer}>
+      <Image
+        style={styles.video}
+        source={require("nlfyapp/assets/blackscreen.jpg")}
+      />
+      <View style={styles.playButton}>
+        <Ionicons name="play-circle-sharp" size={65} color="#F26924" />
+      </View>
+    </View>
+  );
 };
