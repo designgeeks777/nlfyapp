7f1a626992e1746bd286bbf1d9c7966201716634
ria4github <ria.guhat@gmail.com>
2023-02-17 11:50:05 +0530

    Styling change with dimensions

--- nlfyapp/src/features/home.screen.js
+++ nlfyapp/src/features/home.screen.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { ScrollView, View } from "react-native";
+import { ScrollView, View, Dimensions } from "react-native";
 import styled from "styled-components";
 import { Devotional } from "./component/homeScreenComponent/devotional.component";
 import { Sermon } from "./component/homeScreenComponent/sermon.component";
 import { Event } from "./component/homeScreenComponent/event.component";
 import { Welcome } from "./component/homeScreenComponent/welcome.component";
+
+const { width } = Dimensions.get("window");
+const wrapperWidth = width * 0.9;
 const WrapperView = styled(View)`
   top: 40px;
-  width: 360px;
+  width: ${wrapperWidth}px;
   border-radius: 10px;
   padding-bottom: 200px;
 `;
