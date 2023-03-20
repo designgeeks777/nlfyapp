0cda87c50f4b7daa2e68eee2dc6e4e5d1ae3d48b
SephoraS <125300269+SephoraS@users.noreply.github.com>
2023-03-20 17:48:44 +0530

    Delete APIKeys.js

--- nlfyapp/APIKeys.js
+++
@@ -1,8 +0,0 @@
-export const FIREBASECONFIG = {
-  apiKey: "AIzaSyAYd56uq4i5RLjfK6CWuZMCQLU4H5QVP04",
-  authDomain: "fbnlfyapp.firebaseapp.com",
-  projectId: "fbnlfyapp",
-  storageBucket: "fbnlfyapp.appspot.com",
-  messagingSenderId: "1001905609620",
-  appId: "1:1001905609620:web:71e19c63fb773089b88d81",
-};
