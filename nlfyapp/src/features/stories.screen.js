a1f33645cb9b6d4031169d5eaae643cf954f1def
SephoraS <125300269+SephoraS@users.noreply.github.com>
2023-04-14 19:09:08 +0530

    Expand/Collapse API integration for Devotionals and Stories

--- nlfyapp/src/features/stories.screen.js
+++ nlfyapp/src/features/stories.screen.js
@@ -1,15 +1,9 @@
 import React from "react";
-import {
-  View,
-  Dimensions,
-  StatusBar,
-  SafeAreaView,
-  ScrollView,
-} from "react-native";
-
 import styled from "styled-components";
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { BackButton } from "../components/backButton";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { View, Dimensions, StatusBar, SafeAreaView } from "react-native";
+import { ExpandCollapseList } from "../components/expandCollapse.component";
 
 const { width } = Dimensions.get("window");
 const wrapperWidth = width * 0.9;
@@ -17,7 +11,6 @@ const wrapperWidth = width * 0.9;
 const WrapperView = styled(View)`
   width: ${wrapperWidth}px;
   border-radius: 10px;
-  padding-bottom: 400px;
   top: 20px;
   margin-left: 10px;
 `;
@@ -25,16 +18,17 @@ const WrapperView = styled(View)`
 const SafeAreaViewWrapper = styled(SafeAreaView)`
   flex: 1;
   margin-top: ${StatusBar.currentHeight}px;
+  justify-content: flex-start;
 `;
+
 export const Stories = () => {
   return (
     <>
       <SafeAreaViewWrapper>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <WrapperView>
-            <BackButton text="Stories" />
-          </WrapperView>
-        </ScrollView>
+        <WrapperView>
+          <BackButton text="Stories" />
+        </WrapperView>
+        <ExpandCollapseList screenName="stories" />
       </SafeAreaViewWrapper>
       <ExpoStatusBar style="auto" />
     </>
