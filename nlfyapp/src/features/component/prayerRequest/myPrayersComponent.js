1ce2c1a208e6efbec117b5f05b8686296e81e775
ria4github <ria.guhat@gmail.com>
2023-05-05 19:44:22 +0530

    my prayers added pending scrollable modal and displaying responses in modal

--- nlfyapp/src/features/component/prayerRequest/myPrayersComponent.js
+++ nlfyapp/src/features/component/prayerRequest/myPrayersComponent.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { View, Text, Dimensions, SafeAreaView, StatusBar } from "react-native";
 import { BackButton } from "../../../components/backButton";
 import { TabButton } from "../../../components/tabButton";
 import { TabButtonUnselected } from "../../../components/TabButtonUnselected";
 import styled from "styled-components";
 
+import axios from "axios";
+import { BASEURL } from "../../../../APIKey";
+
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+
+import { ExpandCollapseList } from "../../../components/expandCollapse.myPrayerComponent";
+
 import { useNavigation } from "@react-navigation/native";
 
 const { width } = Dimensions.get("window");
 const wrapperWidth = width * 0.9;
+const marginRight = width * 0.05;
 
 const WrapperView = styled(View)`
   width: ${wrapperWidth}px;
@@ -22,14 +30,56 @@ const SafeAreaViewWrapper = styled(SafeAreaView)`
   padding-top: ${StatusBar.currentHeight}px;
   margin-top: -15px;
   z-index: 2;
-  margin-right: 2px;
 `;
 
 const ButtonsWrapper = styled(View)`
   flex-direction: row;
+  justify-content: center;
+  margin-right: ${marginRight}px;
 `;
 
 export const MyPrayersScreen = () => {
+  const url = `${BASEURL}prayerRequests`;
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { user } = useContext(AuthenticationContext);
+
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+    const loadData = async () => {
+      try {
+        const response = await axios.get(url, {
+          cancelToken: source.token,
+        });
+
+        const filteredData = response.data.filter(
+          (record) => record.raisedByUid === user.uid
+        );
+        //console.log("Filtered Data", filteredData);
+
+        setData(filteredData);
+
+        setIsLoading(false);
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          console.log("Request canceled");
+        } else {
+          console.error(error);
+        }
+      }
+    };
+
+    loadData();
+
+    const intervalId = setInterval(loadData, 60000);
+
+    return () => {
+      clearInterval(intervalId);
+      source.cancel("Component unmounted");
+    };
+  }, [url, user.uid]);
+
   const navigation = useNavigation();
   const navigateToChurchPrayers = () => {
     navigation.navigate("ChurchPrayers");
@@ -55,7 +105,9 @@ export const MyPrayersScreen = () => {
             />
             <TabButton label="My Prayers" />
           </ButtonsWrapper>
-          <Text>Content</Text>
+          <View>
+            <ExpandCollapseList data={data} />
+          </View>
         </View>
       </SafeAreaViewWrapper>
     </>
