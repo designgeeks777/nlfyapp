a8ff42cbf06fd0a5971d4ea10855a9da3c5be2ce
ria4github <ria.guhat@gmail.com>
2023-06-09 16:53:38 +0530

    Pre stored value cleared

--- nlfyapp/src/services/authentication/authentication.context.js
+++ nlfyapp/src/services/authentication/authentication.context.js
@@ -79,9 +79,11 @@ export const AuthenticationContextProvider = ({ children }) => {
     setConfirm(null);
   };
   const onSignInWithPhoneNumber = async (phoneNumber, appVerifier) => {
-    //phoneNumber = testPhoneNumber;
+    phoneNumber = testPhoneNumber;
     console.log("SIGN IN AUTH CONTEXT", phoneNumber);
     setIsLoading(true);
+    //This is set to avoid pre-stored value
+    setIsValidOTPCode(false);
     try {
       console.log("CALLED>>>>>>>>>>>>>>>>>>>>>>>>.");
       await firebase
@@ -113,7 +115,7 @@ export const AuthenticationContextProvider = ({ children }) => {
     }
   };
   const confirmCode = async (otpCode) => {
-    //otpCode = testOtpCode;
+    otpCode = testOtpCode;
     console.log("CONFIRM OTP AUTH CONTEXT", otpCode);
     setIsLoadingOTP(true);
     try {
