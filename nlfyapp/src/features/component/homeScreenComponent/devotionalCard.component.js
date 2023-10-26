7d4ba3d0f428ea6d1ee1d4f758bee21dc66e69d3
Emman <emmanuelpalli@gmail.com>
2023-10-26 15:17:18 +0530

    Bug 1-Devotionals If header is too long text becoming small, Bug 9-Read more out of card

--- nlfyapp/src/features/component/homeScreenComponent/devotionalCard.component.js
+++ nlfyapp/src/features/component/homeScreenComponent/devotionalCard.component.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { Card, Text } from "react-native-paper";
+import { Card, Text, Button } from "react-native-paper";
 import { Dimensions } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { useNavigation } from "@react-navigation/native";
@@ -20,7 +20,9 @@ const DevotionalCard = styled(Card)`
 `;
 
 const CardTitle = styled(Text)`
+  text-align: left;
   padding-top: ${cardTop * 0.1}px;
+  padding-left: ${cardWidth * 0.04}px;
   color: ${(props) => props.theme.colors.text.inverse};
   font-size: ${(props) => props.theme.fontSizes.body};
   font-weight: ${(props) => props.theme.fontWeights.bold};
@@ -32,14 +34,11 @@ const CardContent = styled(Text)`
   font-size: ${(props) => props.theme.fontSizes.caption};
   font-weight: ${(props) => props.theme.fontWeights.regular};
   font-family: ${(props) => props.theme.fonts.body};
-  width: ${cardWidth * 0.9}px;
   top: ${cardTop * 0.2}px;
 `;
 
 const CardReadmore = styled(Text)`
-  align-self: flex-end;
-  text-align: right;
-  top: ${cardTop * 0.2}px;
+  padding-top: 14px;
   color: ${(props) => props.theme.colors.text.inverse};
   font-size: ${(props) => props.theme.fontSizes.caption};
   font-weight: ${(props) => props.theme.fontWeights.regular};
@@ -78,26 +77,28 @@ export const HomePageDevotionalCard = ({ devotional = {} }) => {
   return (
     <DevotionalCard>
       <StyledLinearGradient
-            start={{ x: 180, y: 0.25 }}
-            end={{ x: 180, y: 1.0 }}
-            colors={
-              (["#D03925", "rgba(242, 36, 36, 1)"],
-              ["#D03925", "rgba(242, 90, 36, 1)"])
-            }
-          >
-        <Card.Content>
-          {data && (
-            <>
-              <CardTitle variant="titleLarge">{data.subject}</CardTitle>
-              <CardContent numberOfLines={5} variant="bodyMedium">
+        start={{ x: 180, y: 0.25 }}
+        end={{ x: 180, y: 1.0 }}
+        colors={
+          (["#D03925", "rgba(242, 36, 36, 1)"],
+          ["#D03925", "rgba(242, 90, 36, 1)"])
+        }
+      >
+        {data && (
+          <>
+            <CardTitle>{data.subject}</CardTitle>
+            <Card.Content>
+              <CardContent variant="bodyMedium" numberOfLines={5}>
                 {data.content}
               </CardContent>
-            </>
-          )}
-        </Card.Content>
-        <CardReadmore variant="bodyMedium" onPress={goToDevotionalsScreen}>
-          Read More...
-        </CardReadmore>
+            </Card.Content>
+          </>
+        )}
+        <Card.Actions>
+          <CardReadmore onPress={goToDevotionalsScreen}>
+            Read More...
+          </CardReadmore>
+        </Card.Actions>
       </StyledLinearGradient>
     </DevotionalCard>
   );
